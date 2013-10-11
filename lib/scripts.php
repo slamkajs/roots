01a3f57e1980b177f9c19f56311584b4f1adecb6
Justin Slamka <jslamka5685@gmail.com>
2013-10-11 17:16:32 -0400

    Committing changes

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0c1d09b12249ae0dfb4da6fe77ea0e9f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8f9a0bcc11ad9ab7ad9270bbe1b7e268');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
